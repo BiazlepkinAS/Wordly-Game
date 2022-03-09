3bddbf64f359c751ec33eb58f50cc9ec0943f16a
ABezlepkin <6abc6@mail.ru>
2022-03-09 11:22:24 +0300

    init commit

---
+++ WordlyGames/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordlyGames
+//
+//  Created by Andrei Bezlepkin on 9.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
+
